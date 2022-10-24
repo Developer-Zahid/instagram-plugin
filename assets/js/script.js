984dce17f901528b8d88258402dd77a3c944a053
Developer-Zahid <coderexpertzahid@gmail.com>
2022-10-24 14:58:38 +0600

    Added image with js

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+let imagesArr = [
+    'http://insta-api.contact-support.co/1.jpeg',
+    'http://insta-api.contact-support.co/2.jpeg',
+    'http://insta-api.contact-support.co/3.jpeg',
+    'http://insta-api.contact-support.co/4.jpeg',
+    'http://insta-api.contact-support.co/5.jpeg'
+];
+
+let instagramContainer = document.querySelector(".instagram-container");
+
+window.addEventListener("DOMContentLoaded", function(){
+    setTimeout(function(){
+        imagesArr.map((item)=>{
+            instagramContainer.innerHTML += `<a href="${item}" target="_blank" style="background-image: url(${item});" class="instagram__imgage"></a>`
+        })
+    },1000)
+})
